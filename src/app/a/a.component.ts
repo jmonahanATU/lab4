e029ee277dc33ab7486cac45d3ad0c98abc6e92a
jmonahanATU <g00409871@atu.ie>
2023-02-24 12:15:00 +0000

    lab4

---
+++ src/app/a/a.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-a',
+  templateUrl: './a.component.html',
+  styleUrls: ['./a.component.css']
+})
+export class AComponent {
+  class:number = 905;
+  college:string = 'atu';
+}
---
+++ src/app/b/b.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-b',
+  templateUrl: './b.component.html',
+  styleUrls: ['./b.component.css']
+})
+export class BComponent {
+  name2:string = 'tom';
+  age2:number = 30;
+
+}
